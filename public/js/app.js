b11eeac91c8bf37e6e0bbac70663573eee5959ed
ChienBV <bui.van.chien@i-enter.asia>
2021-07-13 13:48:37 +0700

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+console.log('Client side javascript file is loaded!')
+
+fetch('http://localhost:8000/weather?address=hanoi').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
